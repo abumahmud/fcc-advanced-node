8d2556ab64c46ec6805cf67eea031a8e520a881b
abdulbaasit alabi <alabiabdulbaasit@gmail.com>
2025-08-19 04:41:40 +0100

    Completed FCC Advanced Node Task 3

--- server.js
+++ server.js
@@ -14,6 +14,16 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.route('/').get((req, res) => {
   res.render('index', { title: 'Hello', message: 'Please log in' })
 });
