5f06037e9418dfbf0b483285f178ef0c1b75d0dc
abdulbaasit alabi <alabiabdulbaasit@gmail.com>
2025-08-20 02:43:26 +0100

    Completed FCC Advanced Node Task 4

--- server.js
+++ server.js
@@ -3,16 +3,14 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const session = require('express-session');
+const passport = require('passport');
+const { ObjectID } = require('mongodb');
 
 const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
-
-fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.use(session({
   secret: process.env.SESSION_SECRET,
@@ -24,11 +22,26 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+fccTesting(app); // For fCC testing purposes
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.route('/').get((req, res) => {
-  res.render('index', { title: 'Hello', message: 'Please log in' })
+  res.render('index', { title: 'Hello', message: 'Please log in' });
+});
+
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  // myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+    done(null, null);
+  // });
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
+  console.log(`Listening on port ${PORT}`);
 });
