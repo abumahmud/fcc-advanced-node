abbee5b35da8cbe2efc415314711b55dc6734191
abdulbaasit alabi <alabiabdulbaasit@gmail.com>
2025-08-24 03:30:43 +0100

    Completed FCC Advanced Node Task 12

--- server.js
+++ server.js
@@ -55,6 +55,7 @@ myDB(async client => {
 
   app.route('/register')
   .post((req, res, next) => {
+    const hash = bcrypt.hashSync(req.body.password, 12);
     myDataBase.findOne({ username: req.body.username }, (err, user) => {
       if (err) {
         next(err);
@@ -63,7 +64,7 @@ myDB(async client => {
       } else {
         myDataBase.insertOne({
           username: req.body.username,
-          password: req.body.password
+          password: hash
         },
           (err, doc) => {
             if (err) {
@@ -95,7 +96,9 @@ myDB(async client => {
       console.log(`User ${username} attempted to log in.`);
       if (err) { return done(err); }
       if (!user) { return done(null, false); }
-      if (password !== user.password) { return done(null, false); }
+      if (!bcrypt.compareSync(password, user.password)) { 
+        return done(null, false);
+      }
       return done(null, user);
     });
   }));
