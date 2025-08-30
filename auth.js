dfecf0b4f67d007adca757d11ea34038de8870f9
abdulbaasit alabi <alabiabdulbaasit@gmail.com>
2025-08-30 01:53:37 +0100

    Reset OAuth

--- auth.js
+++ auth.js
@@ -31,7 +31,7 @@ module.exports = function (app, myDataBase) {
   passport.use(new GitHubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: 'https://fcc-advanced-node-trq5.onrender.com/auth/github/callback'
+    callbackURL: 'http://localhost:3000/auth/github/callback'
     },
     function (accessToken, refreshToken, profile, cb) {
       console.log(profile);
